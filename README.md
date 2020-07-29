32e0ae54a471d6467f11c45411f9867677f049df
romans <roman233233@gmail.com>
2020-07-29 11:34:57 +0100

    chore: added readme and some additional settings

---
+++ README.md
@@ -0,0 +1,11 @@
+# Graphy Fullstack Challenge
+
+## Instructions
+
+See https://github.com/graphy-engineering/graphy-fullstack-challenge
+
+## Getting started
+
+- Run `yarn` to install dependencies
+- To develop locally, run `yarn compile:watch` and `yarn dev` side by side
+- To run tests, run `yarn test` or `yarn test:watch`
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (_, res) => res.send('Hello World!'));
 
